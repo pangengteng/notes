1af9d772ed97d815a24c025f3dc32ad7ea7fa69b
pangt <pangt@silvrr.com>
2025-01-10 19:24:01 +0800

    排序

--- array_learning.py
+++ array_learning.py
@@ -1,7 +1,14 @@
-i = 0
-l = []
-for _ in range(3):
-    l.append(i)
-    i += 1
+def insertSort(array:[int]): # type: ignore
+    if len(array) == 0 or len(array) == 1:
+        return
+    for i in range(1, len(array)):
+        value = array[i]
+        for j in range(i-1, -1, -1):
+            if array[j] < value:
+                break
+            array[j+1] = array[j]
+        array[j+1] = value
 
-print(l)  # [0, 1, 2]
+array = [3, 2, 1, 4, 5]
+insertSort(array)
+print(array)
