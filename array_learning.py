3593593346d55ac2c58a7e34494109b7dee5264d
pangengteng <panpan.gt@gmail.com>
2025-01-11 17:26:05 +0800

    排序

--- array_learning.py
+++ array_learning.py
@@ -1,3 +1,13 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    
+    def __repr__(self):
+        return repr(self.val) + '->' + repr(self.next)
+
 def insertSort(array:[int]): # type: ignore
     if len(array) == 0 or len(array) == 1:
         return
@@ -9,6 +19,60 @@ def insertSort(array:[int]): # type: ignore
             array[j+1] = array[j]
         array[j+1] = value
 
-array = [3, 2, 1, 4, 5]
-insertSort(array)
-print(array)
+
+# def selectionSort(array: Optional[int]):
+#     for i in range(len(array)-1):
+#         min_value = array[i]
+#         for j in range(i+1, len(array)):
+#             if array[j] < min_value:
+#                 min_value, array[i], array[j] = array[j], array[j], min_value
+
+
+# def selectionSort(head: Optional[ListNode]) -> Optional[ListNode]:
+#     dummy = ListNode(next = head)
+#     result = cur = ListNode()
+    
+#     while dummy.next:
+#         p = dummy
+#         i = j = dummy.next
+#         min_value = i.val
+#         while j.next:
+#             if j.next.val < min_value:
+#                 min_value = j.next.val
+#                 p = j
+#             j = j.next
+        
+#         node = p.next
+#         p.next = p.next.next
+#         node.next = None
+#         cur.next = node
+#         cur = cur.next
+
+#     return result.next
+
+def selectionSort(head: Optional[ListNode]) -> Optional[ListNode]:
+    """ in-place implement """
+    dummy = ListNode(next=head)
+
+    p1 = dummy
+    while p1.next:
+        i = j = p2 = p1.next
+        min_value = i
+        while j.next:
+            if j.next.val < min_value.val:
+                min_value = j.next
+                p2 = j
+            j = j.next
+        
+        if i != min_value:
+            k = p2.next
+            p1.next, p2.next = k, i
+            i.next, k.next = k.next, i.next
+        
+        p1 = p1.next
+    
+    return dummy.next
+
+listNode = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
+result = selectionSort(listNode)
+print(result)
