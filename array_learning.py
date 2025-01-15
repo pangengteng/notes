0d9dd2bf95d678f6ab6ac06c0b1eb8a9445b02ab
pangt <pangt@silvrr.com>
2025-01-15 11:27:21 +0800

    排序

--- array_learning.py
+++ array_learning.py
@@ -8,16 +8,61 @@ class ListNode:
     def __repr__(self):
         return repr(self.val) + '->' + repr(self.next)
 
-def insertSort(array:[int]): # type: ignore
+# def insertSort(array:[int]): # type: ignore
+#     if len(array) == 0 or len(array) == 1:
+#         return
+#     for i in range(1, len(array)):
+#         value = array[i]
+#         for j in range(i-1, -1, -1):
+#             if array[j] < value:
+#                 break
+#             array[j+1] = array[j]
+#         array[j+1] = value
+
+def insert_sort(head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head: # or not head.next:
+        return head
+    dummy = ListNode(next=head)
+    
+    tail = head
+    cur = head.next
+    while cur:
+        if tail.val <= cur.val:
+            tail = cur
+        else:
+            prev = dummy
+            while prev.next.val <= cur.val: # prev.next and prev.next <= cur.next:
+                prev = prev.next
+            
+            tail.next = cur.next
+            cur.next = prev.next
+            prev.next = cur
+        
+        cur = tail.next
+    return dummy.next
+
+
+def insertion_sort(array:Optional[int]):
     if len(array) == 0 or len(array) == 1:
         return
+
+    def _binary_search(array, val, start, end):
+        if start == end:
+            return end
+        mid = start + (end - start) // 2
+        if array[mid] <= val:
+            return _binary_search(array, val, mid+1, end)
+        else:
+            return _binary_search(array, val, start, mid)
+
+
     for i in range(1, len(array)):
         value = array[i]
-        for j in range(i-1, -1, -1):
-            if array[j] < value:
-                break
-            array[j+1] = array[j]
-        array[j+1] = value
+        # binary search
+        j = _binary_search(array, value, 0, i-1)
+        for k in range(i, j, -1):
+            array[k] = array[k-1]
+        array[j] = value
 
 
 # def selectionSort(array: Optional[int]):
@@ -74,7 +119,7 @@ def selectionSort(head: Optional[ListNode]) -> Optional[ListNode]:
     return dummy.next
 
 # listNode = ListNode(4, ListNode(2, ListNode(1, ListNode(3))))
-# result = selectionSort(listNode)
+# result = insert_sort(listNode)
 # print(result)
 
 import sys
@@ -103,26 +148,26 @@ def mergeSort(array:Optional[int]) -> Optional[int]:
     return tmp
 
 
-def quick_sort(array:Optional[int]):
+# def quick_sort(array:Optional[int]):
 
-    def _partition(array, start, end):
-        pivot = array[end]
-        i = 0
-        for j in range(0, end):
-            if array[j] < pivot:
-                array[i], array[j] = array[j], array[i]
-                i += 1
-        array[i], array[end] = array[end], array[i]
-        return i
+#     def _partition(array, start, end):
+#         pivot = array[end]
+#         i = 0
+#         for j in range(0, end):
+#             if array[j] < pivot:
+#                 array[i], array[j] = array[j], array[i]
+#                 i += 1
+#         array[i], array[end] = array[end], array[i]
+#         return i
 
-    def _quick_sort(array:Optional[int], start: Optional[int], end: Optional[int]):
-        if start >= end:
-            return
-        p = _partition(array, start, end)
-        _quick_sort(array, start, p-1)
-        _quick_sort(array, p, end)    
+#     def _quick_sort(array:Optional[int], start: Optional[int], end: Optional[int]):
+#         if start >= end:
+#             return
+#         p = _partition(array, start, end)
+#         _quick_sort(array, start, p-1)
+#         _quick_sort(array, p, end)    
 
-    _quick_sort(array, 0, len(array)-1)
+#     _quick_sort(array, 0, len(array)-1)
 
 
 def shell_sort(array:Optional[int]):
@@ -138,6 +183,41 @@ def shell_sort(array:Optional[int]):
             array[j] = value
         gap = gap // 2
 
-array = [3, 2, 1, 4, 5]
-shell_sort(array)
+array = [9, 8, 1, 0, 3, 2, 7, 6, 5, 4]
+
+
+def quick_sort(array:Optional[int]):
+
+    def _partition(array, start, end):
+        pivot = array[end]
+        i = 0
+        for j in range(0, end):
+            if array[j] < pivot:
+                array[i], array[j] = array[j], array[i]
+                i += 1
+        array[i], array[end] = array[end], array[i]
+        return i
+    
+    stack = [(array, 0, len(array)-1)]
+    
+    while stack:
+        array, start, end = stack.pop()
+        if start >= end:
+            continue
+        q = _partition(array, start, end)
+        stack.append((array, start, q-1))
+        stack.append((array, q+1, end))
+
+def partition(array, idx):
+    pivot = array[idx]
+    i = 0
+    for j in range(len(array)):
+        if array[j] < pivot:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+    array[i], array[idx] = array[idx], array[i]
+    return i
+
+
+insertion_sort(array)
 print(array)
