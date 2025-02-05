c3473be33c1d5822bcbaf1eb123b53d4978509f3
gengtengpan <pangt@silvrr.com>
2025-02-05 21:45:59 +0800

    feat: add KMP algorithm implementation and documentation; update skip list and queue sections

--- test.py
+++ test.py
@@ -1,73 +1,19 @@
-from typing import List
-
-
-def first_index_of(array, value):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = low + ((high - low) >> 1)
-        if array[mid] >= value:
-            high = mid - 1
+def kmp(self, txt: str, patt: str) -> int:
+    new_string = patt + '#' + txt
+    pi = [0] * len(new_string)
+    i = 1
+    k = 0
+    while i < len(new_string):
+        if new_string[i] == new_string[k]:
+            k += 1
+            pi[i] = k
+            i += 1
         else:
-            low = mid + 1
-    
-    if low < len(array) and array[low] == value:
-        return low
-    
-    return -1
+            if k > 0:
+                k = pi[k - 1]
+            else:
+                pi[i] = 0
+                i += 1
+    return pi[-1]
 
-
-def last_index_of(array, value):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = low + ((high - low) >> 1)
-        if array[mid] <= value:
-            low = mid + 1
-        else:
-            high = mid - 1
-    if high >= 0 and array[high] == value:
-        return high
-    return -1
-
-
-def first_gt_index_of(array, value):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = low + ((high - low) >> 1)
-        if array[mid] >= value:
-            high = mid - 1
-        else:
-            low = mid + 1
-    if low < len(array):
-        return low
-    return -1
-
-
-def last_lt_index_of(array, value):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = low + ((high - low) >> 1)
-        if array[mid] <= value:
-            low = mid + 1
-        else:
-            high = mid - 1
-    if high >= 0:
-        return high
-    return -1
-
-def insert_index_of(array, value):
-    low, high = 0, len(array) - 1
-    while low <= high:
-        mid = low + ((high - low) >> 1)
-        if array[mid] <= value:
-            low = mid + 1
-        else:
-            high = mid - 1
-    if low < len(array):
-        return low
-    return -1 # all of elements smaller or equals than value
-
-print(first_index_of([1, 2, 3, 3, 3, 3, 4, 5], 3))  # 2
-print(last_index_of([1, 2, 3, 3, 3, 3, 4, 5], 3))  # 5
-print(first_gt_index_of([1, 2, 3, 3, 3, 3, 4, 5], 3))  # 2
-print(last_lt_index_of([1, 2, 3, 3, 3, 3, 4, 5], 3))  # 5
-print(insert_index_of([1, 2, 3, 3, 3, 3, 4, 5], 3))  # 6
+print(kmp("abxabcabcaby", "abcaby"))  # 6
